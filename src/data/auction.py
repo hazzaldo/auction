5238c3219b55905482dc282aba49540a33cf9046
hazzaldo <hazzaldo@hotmail.com>
2020-06-11 14:28:32 +0100

    add validate_bid method in Auction class

--- src/data/auction.py
+++ src/data/auction.py
@@ -15,7 +15,16 @@ class Auction:
         self.highest_bid=None 
         self.bids=None
 
-    
+    def validate_bid(self, bid) -> str:
+        if bid.auction_id == self.auction_id:
+            if not isinstance(bid.amount, int):
+                return 'Error, bid amount should be of type integer.'
+            elif bid.amount < 0:
+                return 'Error, bid amount is a negative number. It should be a positive number' 
+            else:
+                return ''
+        else:
+            return "Error, the passed bid's auction id does not match the current auction instance's auction id"
 
          
 
