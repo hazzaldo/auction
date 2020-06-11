1ff2ccdac3a479af39329d2adc65de3adc78ca7d
hazzaldo <hazzaldo@hotmail.com>
2020-06-11 23:49:21 +0100

    add test_adding_higher_bid_to_auction test which passed

--- test_app.py
+++ test_app.py
@@ -14,3 +14,15 @@ def test_adding_bid_to_new_auction(create_auction_context):
     auction = create_auction_context.get('auction')
     assert len(auction.bids)
     assert auction.highest_bid
+
+def test_adding_higher_bid_to_auction(create_auction_context):
+    print('\n=> Testing adding a higher bid')
+    auction = create_auction_context.get('auction')
+    bid = create_auction_context.get('bid')
+    new_bid = Bid.generate()
+    new_bid.amount = bid.amount + 1
+    new_bid.auction_id = auction.auction_id
+    message = auction.process_bid(new_bid)
+    assert new_bid == auction.highest_bid
+    assert new_bid in auction.bids
+    assert message == auction.get_accepted_bid_message(new_bid)
