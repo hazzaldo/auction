3a82643e606a8a78b4cdf0da68a50ec6408f4e2b
hazzaldo <hazzaldo@hotmail.com>
2020-06-11 14:27:33 +0100

    add Auction class constructor

---
+++ src/data/auction.py
@@ -0,0 +1,25 @@
+
+class Auction:
+    """
+    This class is responsible for managing an auction's bids.
+
+    """
+
+    def __init__(self, auction_id: int) -> None:
+        """
+        The constructor for the Auction class.
+
+        :param auction_id: (int) the ID of the auction that the bid was on
+        """
+        self.auction_id = auction_id
+        self.highest_bid=None 
+        self.bids=None
+
+    
+
+         
+
+
+
+
+
